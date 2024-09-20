e1fc664a12ca271f946df0cf2653af8496a5500c
shivamgoyal26 <officialshivamgoyal@gmail.com>
2024-09-20 14:07:43 +0530

    fonts implementation done

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,14 @@
+import { Home } from "lucide-react-native";
 import React from "react";
 import { Text, View } from "react-native";
 
 const Login = () => {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-lg font-raleway-bold">Login screen</Text>
+      <Text className="text-lg font-raleway-regular text-foreground">
+        Login screen
+      </Text>
+      <Home className="h-10 w-10 text-primary" />
     </View>
   );
 };
